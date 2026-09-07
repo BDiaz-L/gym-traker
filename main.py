b5c98af6d7fb6c9aae4ab65b163d70864a05c062
BDiaz-L <bantonio.diaz@hotmail.com>
2026-09-07 08:37:44 -0600

    add persist menu

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 
 import tracker
 
-
 def exit_program():
-    print('Saliendo...')
+    print('Saliendo...') 
 
 options = {
-    '1': tracker.training_register,
-    '2': exit_program
+    '1': tracker.training_register
 }
 
-welcome_text = 'Bienvenido, ingresa una de las opciones:\n 1) Registrar Entrenamiento\n 2) Salir\n'
+welcome_text = (
+    'Bienvenido, ingresa una de las opciones:\n'
+    ' 1) Registrar Entrenamiento\n'
+    ' q) Salir\n'
+)
 
-option = input(welcome_text)
-function = options.get(option)
+while True:
+    option = input(welcome_text)
+    if option == 'q':
+        exit_program()
+        break;
+    
+    function = options.get(option)
 
-if function:
-    function()
-else:
-    print('No elegiste una opcion valida')
+    if function:
+        function()
+    else:
+        print('No elegiste una opcion valida')
